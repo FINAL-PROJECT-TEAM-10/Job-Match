cb820a52b4104fe66ea0348d9d62e23f1fd6b3f0
BorislavBonevAlpha <gansdhika1@gmail.com>
2023-11-09 14:11:48 +0200

    Adding structure

---
+++ job-match-app/data/database.py
@@ -0,0 +1,39 @@
+from private_details import *
+from mariadb import connect
+from mariadb.connections import Connection
+
+def _get_connection() -> Connection:
+    return connect(
+        user=my_username,
+        password= my_password,
+        host=my_host,
+        port=3306,
+        database='job_match'
+    )
+
+
+def read_query(sql: str, sql_params=()):
+    with _get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, sql_params)
+
+        return list(cursor)
+
+
+
+def insert_query(sql: str, sql_params=()) -> int:
+    with _get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, sql_params)
+        conn.commit()
+
+        return cursor.lastrowid
+
+
+def update_query(sql: str, sql_params=()) -> bool:
+    with _get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, sql_params)
+        conn.commit()
+
+        return cursor.rowcount > 0
