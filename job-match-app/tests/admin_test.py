ef0ac3db4250b2842f74ed986131932a4c04a92e
a-script-enjoyer <andrey.filipov@gmail.com>
2023-11-27 13:49:55 +0200

    unittests of admin_services

--- job-match-app/tests/admin_test.py
+++ job-match-app/tests/admin_test.py
@@ -3,16 +3,17 @@ import unittest
 from unittest.mock import Mock, patch, MagicMock
 
 from fastapi import HTTPException
-from fastapi.testclient import TestClient
 
 from common.auth import get_current_user
-from main import app
 from routers import admin_routers
+from services import admin_services
+from services.authorization_services import get_password_hash
+from app_models.admin_models import Admin
 
-mock_admin_services = Mock(spec='services.admin_services')
-mock_admin_services.create_admin = Mock(return_value='Mocked return')
-
-admin_routers.admin_services = mock_admin_services
+# mock_admin_services = Mock(spec='services.admin_services')
+# mock_admin_services.create_admin = Mock(return_value='Mocked return')
+#
+# admin_routers.admin_services = mock_admin_services
 
 
 def fake_admin_payload():
@@ -54,8 +55,8 @@ class AdminRouter_Should(unittest.TestCase):
         # get_current_user = fake_regular_payload()
         new_admin = fake_admin()
 
-        with TestClient(app) as test_client:
-            response = test_client.post('/register')
+        # with TestClient(app) as test_client:
+        #     response = test_client.post('/register')
 
         with self.assertRaises(HTTPException) as e:
             admin_routers.add_admin(new_admin, 'asdQWE123!@#')
@@ -83,28 +84,125 @@ class AdminRouter_Should(unittest.TestCase):
 
 class AdminServices_Should(unittest.TestCase):
     def test_adminExists_returnsTrue_ifAdminFound(self):
-        pass
+        admin = fake_admin()
+        with patch('services.admin_services.read_query') as read_query:
+            read_query.return_value = [(3,)]
+            result = admin_services.admin_exists(admin)
+
+        self.assertEqual(True, result)
 
     def test_adminExists_returnsFalse_ifNoAdmin(self):
-        pass
+        with patch('services.admin_services.read_query') as read_query:
+            admin = fake_admin()
+            read_query.return_value = []
+            result = admin_services.admin_exists(admin)
+
+        self.assertEqual(False, result)
 
     def test_adminExistsById_returnsTrue_ifAdminFound(self):
-        pass
+        with patch('services.admin_services.read_query') as read_query:
+            read_query.return_value = [(3,)]
+            result = admin_services.admin_exists_by_id(3)
+            # Note: even though the return value and the parameter are identical
+            # in reality the logic is handled by the SQL query, so they need not be
+            # in the test. We've kept them identical because it mimics real logic.
+
+        self.assertEqual(True, result)
 
     def test_adminExistsById_returnsFalse_ifNoAdmin(self):
-        pass
+        with patch('services.admin_services.read_query') as read_query:
+            read_query.return_value = []
+            result = admin_services.admin_exists_by_id(3)
+
+        self.assertEqual(False, result)
 
     def test_getAdmin_returnsAdmin(self):
-        pass
+        with patch('services.admin_services.read_query') as read_query:
+            username = 'testing_admin'
+            control_admin = Admin(id=1, username=username, first_name='first_name',
+                                  last_name='last_name', email='email@email.email',
+                                  address='111 address lane', phone='5551234',
+                                  city='Sofia', country='Bulgaria')
+            read_query.return_value = [(1, 'testing_admin', 'first_name', 'last_name',
+                                        'email@email.email', '111 address lane',
+                                        '5551234', 'Sofia', 'Bulgaria')]
+
+            admin = admin_services.get_admin(username)
+
+        self.assertIsInstance(admin, Admin)
+        self.assertEqual(control_admin, admin)
+
 
     def test_getAdmin_returnsNoneIfNoAdmin(self):
-        pass
+        with patch('services.admin_services.read_query') as read_query:
+            username = 'testing_admin'
+            read_query.return_value = []
+            result = admin_services.get_admin(username)
+
+        self.assertEqual(None, result)
 
     def test_getAdminByEmail_returnsAdmin(self):
-        pass
+        with patch('services.admin_services.read_query') as read_query:
+            email = 'email@email.email'
+            control_admin = Admin(id=1, username='testing_admin', first_name='first_name',
+                                  last_name='last_name', email=email,
+                                  address='111 address lane', phone='5551234',
+                                  city='Sofia', country='Bulgaria')
+            read_query.return_value = [(1, 'testing_admin', 'first_name', 'last_name',
+                                        'email@email.email', '111 address lane',
+                                        '5551234', 'Sofia', 'Bulgaria')]
+
+            admin = admin_services.get_admin_by_email(email)
+
+        self.assertIsInstance(admin, Admin)
+        self.assertEqual(control_admin, admin)
 
     def test_getAdminByEmail_returnsNoneIfNoAdmin(self):
-        pass
+        with patch('services.admin_services.read_query') as read_query:
+            email = 'email@email.email'
+            read_query.return_value = []
+            result = admin_services.get_admin_by_email(email)
+
+        self.assertEqual(None, result)
 
     def test_createAdmin_ReturnsAdmin(self):
-        pass
+        created_admin = Admin(username='testing_admin', first_name='first_name',
+                              last_name='last_name', email='email@email.email',
+                              address='111 address lane', phone='5551234',
+                              city='Sofia', country='Bulgaria')
+        password = 'password'
+
+        control_admin = Admin(id=1, username='testing_admin', first_name='first_name',
+                              last_name='last_name', email='email@email.email',
+                              address='111 address lane', phone='5551234',
+                              city='Sofia', country='Bulgaria')
+
+        with patch('services.admin_services.insert_query') as insert_query:
+            with patch('services.admin_services.find_location_id') as find_location_id:
+                find_location_id.return_value = 1
+                insert_query.return_value = 1
+
+                admin_services.create_admin(created_admin, password)
+
+        self.assertIsInstance(created_admin, Admin)
+        self.assertEqual(control_admin, created_admin)
+
+    def test_CreateAdmin_HashesPassword(self):
+        created_admin = Admin(username='testing_admin', first_name='first_name',
+                              last_name='last_name', email='email@email.email',
+                              address='111 address lane', phone='5551234',
+                              city='Sofia', country='Bulgaria')
+        password = 'password'
+
+        with patch('services.admin_services.insert_query') as insert_query:
+            with patch('services.admin_services.find_location_id') as find_location_id:
+                find_location_id.return_value = 1
+                insert_query.return_value = 1
+
+                with patch('services.authorization_services.get_password_hash') as get_password_hash:
+                    admin_services.create_admin(created_admin, password)
+
+        get_password_hash.assert_called_once_with(password)
+
+
+
