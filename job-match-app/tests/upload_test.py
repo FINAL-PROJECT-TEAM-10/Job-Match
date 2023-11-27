995295db4c088762f0583be8f2388fdda7ec6196
a-script-enjoyer <andrey.filipov@gmail.com>
2023-11-27 15:07:14 +0200

    testing for is_file_jpeg

--- job-match-app/tests/upload_test.py
+++ job-match-app/tests/upload_test.py
@@ -1,5 +1,9 @@
 import unittest
+import os
+from io import BytesIO
 from unittest.mock import Mock, patch
+
+from fastapi import UploadFile
 
 from services import upload_services
 
@@ -11,10 +15,46 @@ class UploadServices_Should(unittest.TestCase):
         pass
 
     def test_isFileJpeg_TrueIfJPG(self):
-        pass
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        subpath = 'images/JPG.jpg'
+        file_path = os.path.join(cwd, subpath)
+
+        with open(file_path, 'rb') as file:
+            file_data = file.read()
+
+        binary_object = BytesIO(file_data)
+        upload_file = UploadFile(binary_object)
+
+        result = upload_services.is_file_jpeg(upload_file)
+
+        self.assertEqual(True, result)
 
     def test_isFileJpeg_FalseIfNotJPG(self):
-        pass
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        subpath = 'images/PNG.png'
+        file_path = os.path.join(cwd, subpath)
 
-    def test_isFileJpeg_ReturnErrorIfException(self):
-        pass
+        with open(file_path, 'rb') as file:
+            file_data = file.read()
+
+        binary_object = BytesIO(file_data)
+        upload_file = UploadFile(binary_object)
+
+        result = upload_services.is_file_jpeg(upload_file)
+
+        self.assertEqual(False, result)
+
+    def test_isFileJpeg_RaisesExceptionIfNotImage(self):
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        subpath = 'images/TEXT.txt'
+        file_path = os.path.join(cwd, subpath)
+
+        with open(file_path, 'rb') as file:
+            file_data = file.read()
+
+        binary_object = BytesIO(file_data)
+        upload_file = UploadFile(binary_object)
+
+        result = upload_services.is_file_jpeg(upload_file)
+
+        self.assertRaises(Exception)
