ee12f7bfd7e015797bc54cbe81cccbb1a2bf444d
a-script-enjoyer <andrey.filipov@gmail.com>
2023-12-02 16:09:33 +0200

    quitting on adding structure for unittests, will start writing tests directly because of time-constraints and peculiarities of separate tests

--- job-match-app/tests/company_matching_test.py
+++ job-match-app/tests/company_matching_test.py
@@ -37,6 +37,53 @@ class CompanyMatching_Should(unittest.TestCase):
     def test_pendingCvsReturnsDictInList_IfMiniCv(self):
         pass
 
-    # TODO: Finish structure of unit test after optimizing database interactions
+    def test_miniCvDescriptionReturnsString(self):
+        pass
 
+    def test_miniCvDescriptionReturnsNone_IfNoDescription(self):
+        pass
 
+    def test_miniCvMiniSalaryReturnsInt(self):
+        # TODO: Check if database returns int by default
+        pass
+
+    def test_miniCvMiniSalaryReturnsNone_IfNoMinSalary(self):
+        pass
+
+    def test_miniCvMaxSalaryReturnsInt(self):
+        # TODO: Check if database returns int by default
+        pass
+
+    def test_miniCvMaxSalaryReturnsNone_IfNoMaxSalary(self):
+        pass
+
+    def test_miniCvDateCreationReturnsDate(self):
+        pass
+
+    def test_miniCvDateCreationReturnsNone_IfNoDate(self):
+        pass
+
+    def test_cancelRequest_RaisesOkExceptionWhenSuccessful(self):
+        pass
+
+    # TODO: Below might not be in final version
+    def test_cancelRequest_RaisesExceptionWhenNotSuccessful(self):
+        pass
+
+    def test_checkIfCanceledReturnsTrue_IfCancelled(self):
+        pass
+
+    def test_checkIfCanceledReturnsFalse_IfNotCanceled(self):
+        pass
+
+    def test_checkRequestExistsReturnsTrue_IfExists(self):
+        pass
+
+    def test_checkRequestExistsReturnsFalse_IfNotFalse(self):
+        pass
+
+    def test_successfulMatchesReturnsDictInList_IfMatches(self):
+        pass
+
+    def test_successfulMatchesReturnsNone_IfNoMatches(self):
+        pass
---
+++ job-match-app/tests/job_ads_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from unittest.mock import Mock, patch
+
+from services import job_ads_services
+
+class JobAdsServices_Should(unittest.TestCase):
+
+    def test_findCompanyReturnsListWithTuples(self):
+        pass
+
+    def test_findCompanyReturnsEmptyList_WhenNotFound(self):
+        pass
+
+    def test_findNameById_ReturnsString(self):
+        pass
+
+    def test_findNameById_ReturnsNone_IfNotFound(self):
+        pass
+
+    def test_createJobAdd_ReturnsJobAd(self):
+        pass
+
+    # TODO: Finish up
